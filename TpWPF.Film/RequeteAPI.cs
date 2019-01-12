f998024705083c0fb24373a5371b49d90173e76d
cara 125 <veron.mael@orange.fr>
2019-01-12 15:21:16 +0100

    second commit

---
+++ TpWPF.Film/RequeteAPI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TpWPF.Film.Model;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace TpWPF.Film
+{
+    public class RequeteAPI
+    {
+        public const string debutRequete = "http://www.omdbapi.com/?apikey=12434fb2&";
+
+        public void GetByTitre(string titre)
+        {
+            var requete = debutRequete + "t=" + titre;
+            var result = Appel(requete);
+            TransformJsonToObject(result);
+        }
+
+        static string Appel(string requete)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // la requête
+                using (HttpResponseMessage response = client.GetAsync(requete).Result)
+                {
+
+                    using (HttpContent content = response.Content)
+                    {
+                        // récupère la réponse, il ne resterai plus qu'à désérialiser
+                        string result = content.ReadAsStringAsync().Result;
+                        return result;
+                    }
+                }
+            }
+        }
+
+        public void TransformJsonToObject(string json)
+        {
+            var deserializedProduct = JsonConvert.DeserializeObject<FilmModel>(json);
+        }
+    }
+}
---
+++ TpWPF.Film/ViewModels/RechercheFilmsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TpWPF.MVVM;
+
+namespace TpWPF.Film.ViewModels
+{
+    public class RechercheFilmsViewModel : ObservableObject
+    {
+        private RelayCommand rechercherCommand;
+        private RequeteAPI requeteAPI;
+
+        public RechercheFilmsViewModel()
+        {
+            RechercherCommand = new RelayCommand(RechercherCommandExecute);
+            requeteAPI = new RequeteAPI();
+        }
+
+        public RelayCommand RechercherCommand { get => rechercherCommand; set => rechercherCommand = value; }
+
+        public void RechercherCommandExecute(object commandParameter)
+        {
+           requeteAPI.GetByTitre("Pirate");
+        }       
+    }
+}
