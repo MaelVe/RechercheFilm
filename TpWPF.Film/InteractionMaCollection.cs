e869e53d6fd986b93883dc4e8f8889c6535c819c
cara 125 <veron.mael@orange.fr>
2019-01-24 16:20:30 +0100

    commit

---
+++ TpWPF.Film/InteractionMaCollection.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TpWPF.Film.Model;
+
+namespace TpWPF.Film
+{
+    /// <summary>
+    /// Classe d'interaction avec le fichier json contenant une collection de films
+    /// </summary>
+    public class InteractionMaCollection
+    {
+        public void AddToMyCollection(MaCollectionModel maCollectionModel)
+        {
+            using (StreamWriter file = File.CreateText(@"../../Ressources/MaCollection.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, maCollectionModel);
+            }
+        }
+
+        public void GetMyCollection()
+        {
+            MaCollectionModel myCollections;
+            using (StreamReader file = File.OpenText(@"../../Ressources/MaCollection.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                myCollections = (MaCollectionModel)serializer.Deserialize(file, typeof(MaCollectionModel));
+            }
+
+            RequeteAPI requeteAPI = new RequeteAPI();
+            requeteAPI.ConstructionRequete(myCollections.ImdbId);
+
+        }
+    }
+}
---
+++ TpWPF.Film/Model/MaCollectionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpWPF.Film.Model
+{
+    public class MaCollectionModel
+    {
+        private string imdbId;
+
+        public string ImdbId { get => imdbId; set => imdbId = value; }
+    }
+}
